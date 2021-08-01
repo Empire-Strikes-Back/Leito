81b051523559d83f01f85f76b0a19d1aa301f81f
ged <ged@program>
2021-08-01 05:04:46 +0300

    from program.sh i remove no-history push

--- program.sh
+++ program.sh
@@ -53,16 +53,4 @@ server(){
     # yarn server
 }
 
-push(){
-  ORIGIN=$(git remote get-url origin)
-  rm -rf .git
-  git init -b main
-  git remote add origin $ORIGIN
-  git config --local include.path ../.gitconfig
-  git add .
-  git commit -m "i am ged program"
-  git push -f -u origin main
-}
-
-
 "$@"
