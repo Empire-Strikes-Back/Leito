6bed8e6be2f07566b80ee95e1cc7cd852f2badd0
ged <ged@program>
2021-08-03 14:18:02 +0300

    to program.sh i add j-package

--- program.sh
+++ program.sh
@@ -26,4 +26,31 @@ uberjar(){
   mv out/ged.standalone.jar out/jpackage-input/
 }
 
+j-package(){
+  OS=${1:?"Need OS type (windows/linux/mac)"}
+
+  echo "Starting build..."
+
+  if [ "$OS" == "windows" ]; then
+    J_ARG="--win-menu --win-dir-chooser --win-shortcut"
+          
+  elif [ "$OS" == "linux" ]; then
+      J_ARG="--linux-shortcut"
+  else
+      J_ARG=""
+  fi
+
+  jpackage \
+    --input out/jpackage-input \
+    --dest out \
+    --main-jar ged.standalone.jar \
+    --name "ged" \
+    --main-class clojure.main \
+    --arguments -m \
+    --arguments ged.main \
+    --app-version "1" \
+    $J_ARG
+  
+}
+
 "$@"
